d9ce712ac25881913f442d608fd1e9a39c0dc366
gozica <109421350+gozica@users.noreply.github.com>
2024-03-12 11:44:28 -0400

    app.css home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="home">
+      <div className="content">
+        <div className="name">
+          My name is <span>Nellie</span>
+        </div>
+        <div className="description">
+          I'm your coding whiz by day, diving into true crime and books by
+          night, and always up for some outdoor adventure.
+        </div>
+        < a href="/resume.pdf" target="_blank" rel="noopener noreferrer"> 
+        Download My CV </a>
 
-export default Home
+        <div className="avatar">
+            <div className="card">
+                <img src="/avatar.jpg" alt=""/>
+                <div className="info">
+                    <div>Software</div>
+                    <div>Engineer</div>
+                    <div>Front End</div>
+                    <div>Focus</div>
+                  
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
